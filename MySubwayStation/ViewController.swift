39c21ca607478092ae8d7c85917fd45e3ebe5c46
prettyturtle <unifconti@gmail.com>
2021-12-22 22:41:04 +0900

    초기 설정

---
+++ MySubwayStation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySubwayStation
+//
+//  Created by yc on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
